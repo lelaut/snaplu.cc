5981e02067ddea0d591fa54f5602470f7d8e1057
leonteix <leonardo.a.teixeira@exxonmobil.com>
2023-03-01 07:31:50 -0300

    add fake test

---
+++ src/server/api/routers/collection.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment node
+ */
+
+import { sum } from "./collection";
+
+describe("test add function", () => {
+  it("should return 15 for add(10,5)", () => {
+    expect(sum(10, 5)).toBe(15);
+  });
+  it("should return 5 for add(2,3)", () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+});
